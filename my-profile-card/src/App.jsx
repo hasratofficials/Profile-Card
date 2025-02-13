3e0db9988eb6da4244217a1186d98e06f06e5799
QAISER ALI <qaisers.official@gmail.com>
2025-02-13 17:07:39 +0500

    add instruction for the viewers

--- my-profile-card/src/App.jsx
+++ my-profile-card/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   <>
   <div className="flex flex-wrap justify-center gap-4 mt-4 ">
   <ProfileCard name="Qaiser Ali" rollnumber="23027119-003" email="qaisers.official@gmail.com" image="/me.jpg"/>
+  {/* You can change this photo into you're and can create you card */}
   <ProfileCard name="Noorullah" rollnumber="23027119-052" email="noorullah1234@gmail.com" image="/me.jpg"/>
   </div>
   </>
